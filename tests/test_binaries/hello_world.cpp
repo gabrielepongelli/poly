e0a323ef32e6c274bb962f7b290775f311d4f566
Gabriele Pongelli <pongelligabriele@gmail.com>
2022-06-24 12:17:57 +0200

    Fixed linker script for MinGW and a test binary.

--- tests/test_binaries/hello_world.cpp
+++ tests/test_binaries/hello_world.cpp
@@ -1,6 +1,17 @@
+#if (!defined(_WIN32)) && (!defined(_WIN64))
 #include <iostream>
 
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
+#else
+#include <Windows.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+    auto handler = GetCurrentProcess();
+    TerminateProcess(handler, 0);
+}
+#endif
